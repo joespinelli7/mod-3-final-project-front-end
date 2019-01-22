e2ea32ca202b39ef49194fc53ae84ec6ae01582b
github username <github email address>
2019-01-22 17:47:51 -0500

    Put in bootstrap sources

--- index.js
+++ index.js
@@ -3,40 +3,42 @@ document.addEventListener('DOMContentLoaded', getAllTopics())
 function getAllTopics(){
   fetch(`http://localhost:3000/topics`)
   .then(res => res.json())
-  .then(topics => topics.forEach( (topic) => {
-    const newsContainer = document.querySelector('#news-container')
-
-    let newsDiv = document.createElement('div')
-    newsDiv.classList.add('news')
-    newsContainer.appendChild(newsDiv)
-
-    let header1 = document.createElement('h3')
-    header1.innerText = topic.title
-
-    let header2 = document.createElement('h4')
-    header2.innerText = topic.description
-
-    let header3 = document.createElement('h5')
-    header3.innerText = topic.url
-
-    let header4 = document.createElement('h6')
-    header4.innerText = topic.author
-
-    let image = document.createElement('img')
-    image.src = topic.url_to_image
-
-
-
-    newsDiv.appendChild(image)
-    newsDiv.appendChild(header1)
-    newsDiv.appendChild(header2)
-    newsDiv.appendChild(header3)
-    newsDiv.appendChild(header4)
-    newsContainer.appendChild(newsDiv)
-  }))
+  .then(topics => {
+    topics.forEach( (topic) => {
+      renderTopics(topic)
+    })
+  })
 }
 
-// function renderTopics() {
-//   let div = document.createElement('div')
-//   let src = topic.url
-// }
+function renderTopics(topic) {
+const newsContainer = document.querySelector('#news-container')
+const newsRow = document.querySelector('#news-row')
+
+let newsDiv = document.createElement('div')
+newsDiv.classList.add('row')
+newsRow.appendChild(newsDiv)
+newsContainer.appendChild(newsRow)
+
+let header1 = document.createElement('h3')
+header1.innerText = topic.title
+
+let header2 = document.createElement('h4')
+header2.innerText = topic.description
+
+let header3 = document.createElement('h5')
+header3.innerText = topic.url
+
+let header4 = document.createElement('h6')
+header4.innerText = topic.author
+
+let image = document.createElement('img')
+image.src = topic.url_to_image
+
+
+
+newsDiv.appendChild(header1)
+newsDiv.appendChild(image)
+newsDiv.appendChild(header2)
+newsDiv.appendChild(header3)
+newsDiv.appendChild(header4)
+}
