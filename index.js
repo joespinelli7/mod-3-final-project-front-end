2e97c814ea1e8f633113256cbc6229945cfd47f2
github username <github email address>
2019-01-22 14:36:47 -0500

    Figuring out Javascript functions

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ document.addEventListener('DOMContentLoaded', getAllTopics())
 function getAllTopics(){
   fetch(`http://localhost:3000/topics`)
   .then(res => res.json())
-  .then(data => {console.log(data)})
+  .then(topics => topics.forEach( (topic) => {
+    let div = document.createElement('div')
+    let li = document.createElement('li')
+    let src = topic.url
+    li.appendChild(src)
+    div.appendChild(li)
+  }))
+}
+
+function renderTopics() {
+  let div = document.createElement('div')
+  let li = document.createElement('li')
+  let src = topic.url
 }
