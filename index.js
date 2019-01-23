ecee65cfae82476c2fa1d1689f202f8fe85af32a
github username <github email address>
2019-01-22 20:12:46 -0500

    homepage and search button

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-document.addEventListener('DOMContentLoaded', getAllTopics())
+document.addEventListener('DOMContentLoaded', () => {
+  getAllTopics()
+
+  let searchForm = document.querySelector('#new-search-form')
+  searchForm.addEventListener('submit', searchNews)
+})
 
 function getAllTopics(){
-  fetch(`http://localhost:3000/topics`)
+  fetch(`https://newsapi.org/v2/top-headlines?language=en&apiKey=c1787a15125449dca4ac46f1dd0b8a0f`)
   .then(res => res.json())
   .then(topics => {
-    topics.forEach( (topic) => {
-      renderTopics(topic)
+    topics.articles.forEach( (topic) => {
+      renderTopic(topic)
     })
   })
 }
 
-function renderTopics(topic) {
+function renderTopic(topic) {
 const newsContainer = document.querySelector('#news-container')
 
 let newsDiv = document.createElement('div')
@@ -30,7 +35,7 @@ let header4 = document.createElement('h6')
 header4.innerText = topic.author
 
 let image = document.createElement('img')
-image.src = topic.url_to_image
+image.src = topic.urlToImage
 
 
 
@@ -39,4 +44,19 @@ newsDiv.appendChild(image)
 newsDiv.appendChild(header2)
 newsDiv.appendChild(header3)
 newsDiv.appendChild(header4)
+}
+
+function searchNews(e) {
+  e.preventDefault()
+
+  let searchTerm = document.querySelector('#new-search').value
+
+  fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&language=en&apiKey=c1787a15125449dca4ac46f1dd0b8a0f`)
+  .then(res => res.json())
+  .then(data => {
+    document.querySelector('#news-container').innerHTML = ""
+  	data.articles.forEach( (article) => {
+      renderTopic(article)
+    })
+  })
 }
