cc7367b748f08a321e4f9830a4f77399e42cfa13
github username <github email address>
2019-01-25 16:07:09 -0500

    Sports search button works

--- index.js
+++ index.js
@@ -20,16 +20,17 @@ function showCategories() {
   sportsBtn.classList.add('margin-btn')
   sportsBtn.innerText = "Sports 🏈"
   div.appendChild(sportsBtn)
-  // sportsBtn.addEventListener('click', () => {
-  //   fetch(`https://newsapi.org/v2/everything?q=sports&apiKey=${APIKEY}`)
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     data.articles.forEach( (topic) => {
-  //       // debugger
-  //     // renderTopic(topic)
-  //     })
-  //   })
-  // })
+  sportsBtn.addEventListener('click', () => {
+    fetch(`https://newsapi.org/v2/everything?q=sports&apiKey=${APIKEY}`)
+    .then(res => res.json())
+    .then(data => {
+      let newsContainer = document.querySelector('#news-container').innerHTML = ""
+      data.articles.forEach( (topic) => {
+        // debugger
+      renderCategoryTopics(topic)
+      })
+    })
+  })
 
   let entertainmentBtn = document.createElement('button')
   entertainmentBtn.classList.add('margin-btn')
@@ -51,6 +52,112 @@ function showCategories() {
   businessBtn.innerText = "Business 💸"
   div.appendChild(businessBtn)
 }
+
+
+function renderCategoryTopics(topic) {
+  // debugger
+  // const newsContainer = document.querySelector('#news-container').innerHTML = ""
+  let newsContainerNew = document.querySelector('#news-container')
+
+  let navBar = document.querySelector('nav')
+
+  let newsDiv = document.createElement('div')
+  newsDiv.classList.add(`row-${++topicId}`)
+
+  let header1 = document.createElement('h3')
+  header1.innerHTML = `<strong>Title:</strong> ${topic.title}`
+
+  let header2 = document.createElement('h4')
+  header2.innerText = topic.description
+
+  let header3 = document.createElement('h5')
+  // header3.innerText = topic.url
+
+  let aTag = document.createElement('a')
+  let newWindow = document.createAttribute("target")
+  newWindow.value = "_blank"
+  aTag.href = `${topic.url}`
+  aTag.innerText = "Link to article here!"
+  aTag.setAttributeNode(newWindow)
+  header3.appendChild(aTag)
+
+  let header4 = document.createElement('h6')
+
+  if (topic.author === null) {
+    header4.innerHTML = `<hr class="my-${topicId}"><br>`
+  } else {
+    header4.innerHTML = `<strong>Author:</strong> ${topic.author}
+    <hr class="my-${topicId}"><br>`
+  }
+
+  let image = document.createElement('img')
+  if (topic.urlToImage === null){
+    image.innerHTML = `<br><br><br>`
+  } else {
+    image.src = topic.urlToImage
+    image.classList.add('rounded')
+    image.alt = "No Image Available"
+  }
+
+  let br = document.createElement('br')
+
+  //welcomes user when they sign in
+  //add box or highlight to it
+
+                                                                                      //POSSIBLY REMOVE
+  // if (navBar.id !== "0"){
+  //   let welcomeMessage = document.createElement('h1')
+  //   welcomeMessage.id = "welcome"
+  //
+  //
+  //   grabUserName()
+  //   // document.querySelector('#input-id').reset()
+  //   // document.querySelector('#input-btn').innerText = "Login"
+  //   navBar.appendChild(welcomeMessage)
+  //   }
+
+
+  newsDiv.appendChild(header1)
+  newsDiv.appendChild(image)
+  newsDiv.appendChild(header2)
+  newsDiv.appendChild(header3)
+  newsDiv.appendChild(header4)
+  newsContainerNew.appendChild(newsDiv)
+
+  if (navBar.id !== "hi"){
+    let likeButton = document.createElement('button')
+    likeButton.classList.add('like-button')
+    likeButton.innerText = "Like 👍"
+    likeButton.addEventListener('click', saveArticle)
+
+    let removeButton = document.createElement('button')
+    removeButton.classList.add('remove-button')
+    removeButton.innerText = "Remove 👎"
+    removeButton.addEventListener('click', removeMainNewsArticle)
+
+    //LOGOUT!!!!
+
+    let logoutButton = document.getElementById('input-btn')
+    logoutButton.innerText = "Logout"
+    logoutButton.addEventListener('click', document.addEventListener('DOMContentLoaded', init))
+
+    newsDiv.appendChild(likeButton)
+    newsDiv.appendChild(removeButton)
+
+    if (topic.author === null) {
+      header4.innerHTML = `<hr class="my-${topicId}"><br>`
+    } else {
+      header4.innerHTML = `<strong>Author:</strong> ${topic.author}
+      <hr class="my-${topicId}"><br>`
+    }
+
+    newsDiv.appendChild(header4)
+
+  
+  }
+}
+
+
 
 
 function loginForm(){
@@ -209,7 +316,8 @@ function logout(){
 //   source_id: , source_name: , url: , urlToImage: , user_id: )
 
 function saveArticle(e){
-  const author = e.target.parentNode.childNodes[4].innerText
+  const author = e.target.parentNode.childNodes[3].innerText
+  // debugger
   const title = e.target.parentNode.childNodes[0].innerText
   const description = e.target.parentNode.childNodes[2].innerText
   const urlToImage = e.target.parentNode.childNodes[1].src
@@ -217,7 +325,6 @@ function saveArticle(e){
   let navBar = document.querySelector('nav')
   let navBarId = parseInt(navBar.id)
   let parentNode = e.target.parentNode
-  // debugger
   fetch(`http://localhost:3000/topics/`,{
     method: "POST",
     body: JSON.stringify({
